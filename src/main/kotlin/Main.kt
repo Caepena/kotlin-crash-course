20f96e6bd9776c1e8412ec11e9fe4732bcaaa4c4
Caepena <caepenafiel@gmail.com>
2025-10-03 18:39:17 -0300

    O que foi estudado: val x var, operadores aritméticos, operadores de comparação, operadores lógicos, nullability e operadores null-safe, "if"s condicionais.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Place enter a number: ")
+    val input = readln()
+    val inputAsInteger = input.toIntOrNull()
+    if (inputAsInteger != null) {
+        val output = if (inputAsInteger % 2 == 0) {
+            "The number is even!"
+        } else if (inputAsInteger < 10) {
+            "The number is odd and less than 10"
+        } else {
+            "The number is odd and at least 11"
+        }
+
+        println(output)
+    } else {
+        println("Dude, enter a valid number!")
+    }
+}
